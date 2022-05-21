782f49d3e62d0b401e28671684c7a8b8bd313e1c
Jeetendrasinh <jeetendraparmar22@gmail.com>
2022-05-21 19:11:21 +0530

    Add name route
    Signed-off-by: Jeetendrasinh <jeetendraparmar22@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/displayName', [TestController::class, 'displayName']);
 Route::post('/addUser', [TestController::class, 'addUser']);
 Route::post('/addPost', [TestController::class, 'addPost']);
+Route::post('/myname', [TestController::class, 'manageNAMe']);
